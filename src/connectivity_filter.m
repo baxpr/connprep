c84b3572ee2abbf7c2ceebc6f5173838369f7010
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-05-28 15:18:06 -0500

    Working all the way through

--- src/connectivity_filter.m
+++ src/connectivity_filter.m
@@ -1,15 +1,23 @@
-function [filtered_fmri_file,roidata_file,connmat_file] = ...
-	connectivity_filter( ...
+function filtered_fmri_nii = connectivity_filter( ...
 	out_dir, ...
-	badvols, ...
-	rp_file, ...
-	wcseg_file, ...
-	wfmri_file, ...
-	roi_file, ...
-	params, ...
-	remove_gm, ...
+	fmri_nii, ...
+	rp_txt, ...
+	gray_nii, ...
+	white_nii, ...
+	csf_nii, ...
+	tr, ...
+	slorder, ...
+	num_initial_vols_to_drop, ...
+	num_vols_to_analyze, ...
+	bandpasslo_hz, ...
+	bandpasshi_hz, ...
 	FD, ...
 	DVARS, ...
+	badvols, ...
+	mot_PCs, ...
+	motderiv_PCs, ...
+	wmcsf_PCs, ...
+	remove_gm, ...
 	project, ...
 	subject, ...
 	session, ...
@@ -20,6 +28,15 @@ function [filtered_fmri_file,roidata_file,connmat_file] = ...
 %
 % Bandpass filter is implemented as a set of sine and cosine basis
 % functions in the confound matrix.
+
+
+%% Some things are strings
+mot_PCs = str2double(mot_PCs);
+motderiv_PCs = str2double(motderiv_PCs);
+wmcsf_PCs = str2double(wmcsf_PCs);
+bandpasslo_hz = str2double(bandpasslo_hz);
+bandpasshi_hz = str2double(bandpasshi_hz);
+
 
 %% Filename tag based on filtering params
 if remove_gm==0 && isempty(badvols)
@@ -34,98 +51,134 @@ end
 
 
 %% Motion and first differences regressors
-rp = load(char(rp_file));
-if params.mot_PCs == 0
+rp = load(char(rp_txt));
+if mot_PCs == 0
+	fprintf('No motion removed\n')
 	mot_regr = [];
-elseif params.mot_PCs == 6
+elseif mot_PCs == 6
+	fprintf('Remove 6 motion params\n')
 	mot_regr = rp;
 else
+	fprintf('Remove %d motion PCs\n',mot_PCs);
 	[~,P] = pca(zscore(rp));
-	mot_regr = P(:,1:params.mot_PCs);
+	mot_regr = P(:,1:mot_PCs);
 end
 
 rp_delta = [zeros(1,6); diff(rp)];
-if params.motderiv_PCs == 0
+if motderiv_PCs == 0
+	fprintf('No motion derivs removed\n')
 	motderiv_regr = [];
-elseif params.motderiv_PCs == 6
+elseif motderiv_PCs == 6
+	fprintf('Remove 6 motion derivs\n')
 	motderiv_regr = rp_delta;
 else
+	fprintf('Remove %d motion deriv PCs\n',mot_PCs);
 	[~,P] = pca(zscore(rp_delta));
-	motderiv_regr = P(:,1:params.motderiv_PCs);
+	motderiv_regr = P(:,1:motderiv_PCs);
 end
 
 
-%% Resample the w seg image to the fmri voxel geometry
+%% Resample the structural images to the fmri voxel geometry
 flags = struct( ...
 	'mask',true, ...
 	'mean',false, ...
-	'interp',0, ...
+	'interp',1, ...
 	'which',1, ...
 	'wrap',[0 0 0], ...
 	'prefix','r' ...
 	);
-spm_reslice({[wfmri_file ',1']; wcseg_file},flags);
-[p,n,e] = fileparts(wcseg_file);
-rwcseg_file = fullfile(p,['r' n e]);
+
+spm_reslice({[fmri_nii ',1']; gray_nii},flags);
+[p,n,e] = fileparts(gray_nii);
+rgray_nii = fullfile(p,['r' n e]);
+
+spm_reslice({[fmri_nii ',1']; white_nii},flags);
+[p,n,e] = fileparts(white_nii);
+rwhite_nii = fullfile(p,['r' n e]);
+
+spm_reslice({[fmri_nii ',1']; csf_nii},flags);
+[p,n,e] = fileparts(csf_nii);
+rcsf_nii = fullfile(p,['r' n e]);
 
 
-%% Gray, white, CSF regressors from unsmoothed fmri
+% Read images. Reshape fmri for time series processing
+grayV = spm_vol(char(rgray_nii));
+grayY = spm_read_vols(grayV);
 
-% Read images and reshape
-wcsegV = spm_vol(char(rwcseg_file));
-wcsegY = spm_read_vols(wcsegV);
+whiteV = spm_vol(char(rwhite_nii));
+whiteY = spm_read_vols(whiteV);
 
-fmriV = spm_vol(char(wfmri_file));
+csfV = spm_vol(char(rcsf_nii));
+csfY = spm_read_vols(csfV);
+
+fmriV = spm_vol(char(fmri_nii));
 fmriY = spm_read_vols(fmriV);
 fmriY = reshape(fmriY,[],size(fmriY,4))';
 
-% Define GM and WMCSF compartments
-wmcsfY = ismember(wcsegY,[40 41 44 45   4 11 49 50 51 52]);
-gmY = (wcsegY>0) & ~wmcsfY;
+
+% Define GM and WMCSF compartments and write to file
+wmcsf_threshold = 0.98;
+wmcsfY = (whiteY + csfY) >= wmcsf_threshold;
+wmcsfV = whiteV;
+wmcsfV.pinfo(1:2) = [1 0];
+wmcsfV.fname = [out_dir filesep 'wmcsf_mask.nii'];
+spm_write_vol(wmcsfV,wmcsfY);
+
+gray_threshold = 0.5;
+gmY = grayY >= gray_threshold;
+gmV = grayV;
+gmV.pinfo(1:2) = [1 0];
+gmV.fname = [out_dir filesep 'gm_mask.nii'];
+spm_write_vol(gmV,gmY);
+
 
 % Erode WMCSF
-nhood = nan(3,3,3);
-nhood(:,:,1) = [0 0 0; 0 1 0; 0 0 0];
-nhood(:,:,2) = [0 1 0; 1 1 1; 0 1 0];
-nhood(:,:,3) = [0 0 0; 0 1 0; 0 0 0];
-erodeY = wmcsfY;
-erodeY = imerode(erodeY,nhood);
-erodeV = wcsegV;
-erodeV.pinfo(1:2) = [1 0];
-erodeV.fname = [out_dir '/wmcsf_eroded.nii'];
-spm_write_vol(erodeV,erodeY);
+%nhood = nan(3,3,3);
+%nhood(:,:,1) = [0 0 0; 0 1 0; 0 0 0];
+%nhood(:,:,2) = [0 1 0; 1 1 1; 0 1 0];
+%nhood(:,:,3) = [0 0 0; 0 1 0; 0 0 0];
+%erodeY = wmcsfY;
+%erodeY = imerode(erodeY,nhood);
+%erodeV = wcsegV;
+%erodeV.pinfo(1:2) = [1 0];
+%erodeV.fname = [out_dir '/wmcsf_eroded.nii'];
+%spm_write_vol(erodeV,erodeY);
 
 % Gray matter regressor
 if remove_gm
+	fprintf('Removing mean gray matter signal\n')
 	gray_signals = fmriY(:,gmY(:)>0);
 	gray_regr = mean(gray_signals,2);
 else
+	fprintf('NOT removing mean gray matter signal\n')
 	gray_regr = [];
 end
 
 % WMCSF regressors 
-wmcsf_signals = fmriY(:,erodeY(:)>0);
-if params.wmcsf_PCs == 0
+wmcsf_signals = fmriY(:,wmcsfY(:)>0);
+if wmcsf_PCs == 0
+	fprintf('NOT removing white matter/CSF signal\n')
 	wmcsf_regr = [];
-elseif params.wmcsf_PCs == 1
+elseif wmcsf_PCs == 1
+	fprintf('Removing mean white matter/CSF signal\n')
 	wmcsf_regr = mean(wmcsf_signals,2);
 else
-	[~,wmcsf_PCs] = pca(zscore(wmcsf_signals));
-	wmcsf_regr = wmcsf_PCs(:,1:params.wmcsf_PCs);
+	fprintf('Removing %d PCs of white matter/CSF signal\n',wmcsf_PCs)
+	[~,PC] = pca(zscore(wmcsf_signals));
+	wmcsf_regr = PC(:,1:wmcsf_PCs);
 end
-
-% Save memory
-clear wcsegY fmriY
 
 
 %% Bandpass filter regressors
+fprintf('Filtering, %f - %f Hz\n',bandpasslo_hz,bandpasshi_hz);
 bp_regr = fourier_filter_basis( ...
-	size(rp,1),params.tr,params.bandpasslo_hz,params.bandpasshi_hz);
+	size(rp,1),tr,bandpasslo_hz,bandpasshi_hz);
 
 
 %% Scrub regressors
 badvol_regr = zeros(length(badvols),sum(badvols));
 badinds = find(badvols);
+fprintf('Scrubbing %d bad volumes\n',length(badinds));
 for b = 1:length(badinds)
     badvol_regr(badinds(b),b) = 1;
 end
@@ -134,7 +187,7 @@ end
 %% Create and apply confound (filter) matrix
 
 % Read unsmoothed images
-fmriV = spm_vol(char(wfmri_file));
+fmriV = spm_vol(char(fmri_nii));
 fmriY = spm_read_vols(fmriV);
 o = size(fmriY);
 fmriY = reshape(fmriY,[],o(4))';
@@ -169,73 +222,16 @@ randkeeps = keeps(randperm(length(keeps),2500));
 Rpre = ( corr(fmriY(:,randkeeps)) );
 Rpost = ( corr(fmriYc(:,randkeeps)) );
 
-% Save memory
-clear fmriY
-
-
-% Extract ROI signals
-roiV = spm_vol(roi_file);
-roiY = spm_read_vols(roiV);
-
-roi_label = unique(roiY);
-roi_label = roi_label(roi_label~=0);
-
-roi_data = nan(size(fmriYc,1),length(roi_label));
-
-for r = 1:length(roi_label)
-	inds = roiY(:)==roi_label(r);
-	tmp = fmriYc(:,inds);
-	roi_data(:,r) = mean(tmp,2);
-end
-
-% Compute connectivity Z maps
-if params.output_maps
-	
-	Z = atanh(corr(roi_data,fmriYc)) * sqrt(size(roi_data,1)-3);
-	
-	% Write out connectivity maps
-	Z = reshape(Z',[o(1:3) size(roi_data,2)]);
-	filtered_Z_file = fullfile(out_dir,['Zmap_' filetag '.nii']);
-	for v = 1:size(roi_data,2)
-		thisV = rmfield(fmriV(1),'pinfo');
-		thisV.dt(1) = spm_type('float32');
-		thisV.n(1) = v;
-		thisV.fname = filtered_Z_file;
-		spm_write_vol(thisV,Z(:,:,:,v));
-	end
-	
-	% Smooth Z maps
-	smooth(filtered_Z_file,params);
-	
-end
 
 % Write out the filtered unsmoothed images
 fmriYc = reshape(fmriYc',o);
-filtered_fmri_file = fullfile(out_dir,['filtered_' filetag '.nii']);
+filtered_fmri_nii = fullfile(out_dir,['filtered_' filetag '.nii']);
 for v = 1:numel(fmriV)
     thisV = rmfield(fmriV(v),'pinfo');
     thisV.dt(1) = spm_type('float32');
-    thisV.fname = filtered_fmri_file;
+    thisV.fname = filtered_fmri_nii;
     spm_write_vol(thisV,fmriYc(:,:,:,v));
 end
-
-% Save ROI time series to file, marked by the image label
-roi_label_str = strcat('r', strtrim(cellstr(num2str(roi_label))') );
-roi_data = array2table(roi_data,'VariableNames',roi_label_str);
-roidata_file = [out_dir '/roi_timeseries_' filetag '.csv'];
-writetable(roi_data,roidata_file);
-
-% Save memory
-clear fmriYc Z
-
-
-%% Compute connectivity R
-% Mark rows and columns by ROI image label
-connmat = corr(table2array(roi_data));
-connmat = array2table(connmat,'VariableNames',roi_label_str, ...
-	'RowNames',roi_label_str);
-connmat_file = [out_dir '/connectivity_matrix_R_' filetag '.csv'];
-writetable(connmat,connmat_file,'WriteRowNames',true)
 
 
 %% Save a stats file
@@ -251,16 +247,16 @@ statsstr = sprintf( ...
     'wmcsf_PCs=%d\n' ...
     'remove_gray=%d\n' ...
     'slorder=%s\n' ...
-    'init_dropvols=%d\n' ...
-	'kept_vols=%d\n' ...
+    'init_dropvols=%s\n' ...
+	'analyzed_vols=%s\n' ...
 	'scrubbed_vols=%d\n' ...
 	'Final_DOF=%d\n' ...
     ], ...
     nanmedian(FD), nanmedian(DVARS), ...
-    params.bandpasslo_hz,params.bandpasshi_hz, params.tr, ...
-    params.mot_PCs, params.motderiv_PCs, ...
-	params.wmcsf_PCs, remove_gm, ...
-	params.slorder, params.drop_initialvols, params.vols_kept, ...
+	bandpasslo_hz, bandpasshi_hz, tr, ...
+    mot_PCs, motderiv_PCs, ...
+	wmcsf_PCs, remove_gm, ...
+	slorder, num_initial_vols_to_drop, num_vols_to_analyze, ...
 	sum(badvols), size(desmtx,1)-size(desmtx,2) );
 f = fopen([out_dir '/stats_' filetag '.txt'],'wt');
 fprintf(f,statsstr);
@@ -295,22 +291,22 @@ set(figH.summary_text, 'String', sprintf( ...
     'DVARS_median %0.2f\n' ...
     'bandpass_lo %0.3f Hz\n' ...
     'bandpass_hi %0.3f Hz\n' ...
-    'TR %0.3f s\n' ...
+    'TR %0.3d s\n' ...
     'mot_PCs %d\n' ...
     'motderiv_PCs %d\n' ...
     'wmcsf_PCs %d\n' ...
     'remove_gray %d\n' ...
     'slorder %s\n' ...
-    'init_dropvols %d\n' ...
-	'kept_vols %d\n' ...
+    'init_dropvols %s\n' ...
+	'analyzed_vols %s\n' ...
 	'scrubbed_vols %d\n' ...
 	'Final DOF %d\n' ...
     ], ...
     nanmedian(FD), nanmedian(DVARS), ...
-    params.bandpasslo_hz,params.bandpasshi_hz, params.tr, ...
-    params.mot_PCs, params.motderiv_PCs, ...
-	params.wmcsf_PCs, remove_gm, ...
-	params.slorder, params.drop_initialvols, params.vols_kept, ...
+	bandpasslo_hz, bandpasshi_hz, tr, ...
+    mot_PCs, motderiv_PCs, ...
+	wmcsf_PCs, remove_gm, ...
+	slorder, num_initial_vols_to_drop, num_vols_to_analyze, ...
 	sum(badvols), size(desmtx,1)-size(desmtx,2) ))
 
 % Scan info
@@ -322,7 +318,7 @@ set(figH.version,'String',['Matlab version: ' version]);
 
 
 % Compute COM of image in each axis
-fmriV = spm_vol([char(wfmri_file) ',1']);
+fmriV = spm_vol([char(fmri_nii) ',1']);
 fmriY = spm_read_vols(fmriV);
 q3 = squeeze(nansum(nansum(fmriY,1),2));
 fmricom3 = round(sum((1:length(q3))' .* q3) / sum(q3));
@@ -343,16 +339,16 @@ imagesc(imrotate(abs(img),90))
 colormap(gray)
 axis image
 axis off
-title('Grey matter')
+title(sprintf('Grey (%0.0f%%)',100*gray_threshold))
 
 % WM/CSF mask
 axes(figH.wmcsf)
-img = erodeY(:,:,fmricom3);
+img = wmcsfY(:,:,fmricom3);
 imagesc(imrotate(abs(img),90))
 colormap(gray)
 axis image
 axis off
-title('WM + CSF eroded')
+title(sprintf('WM/CSF (%0.0f%%)',100*wmcsf_threshold))
 
 % FD
 axes(figH.FD)
@@ -387,37 +383,3 @@ ylabel('Frequency in random gray matter sample')
 % Print to PNG
 print(gcf,'-dpng',fullfile(out_dir,['connectivity_' filetag '.png']))
 close(gcf);
-
-
-%% Second page of PDF with the connectivity matrix
-
-% Create figure
-pdf_figure = openfig('pdf_connmat_figure.fig','new');
-set(pdf_figure,'Tag','pdf_connmat');
-set(pdf_figure,'Units','pixels','Position',[0 0 dw dh]);
-figH = guihandles(pdf_figure);
-
-% Summary
-set(figH.summary_text, 'String', sprintf( ...
-    'remove_gray %d; scrubbed_vols %d\n', ...
-	remove_gm, sum(badvols) ))
-
-% Scan info
-set(figH.scan_info, 'String', sprintf( ...
-	'%s: %s, %s, %s, %s', ...
-	filetag, project, subject, session, scan));
-set(figH.date,'String',['Report date: ' date]);
-set(figH.version,'String',['Matlab version: ' version]);
-
-% Conn matrix
-axes(figH.connmatrix)
-imagesc(table2array(connmat),[-1 1])
-colormap(jet)
-axis image
-set(gca,'XTickLabelRotation',90)
-
-% Print to PNG
-print(gcf,'-dpng',fullfile(out_dir,['connmatrix_' filetag '.png']))
-close(gcf);
-
-
