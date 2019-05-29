cb3a6cdaac3b69ada19e7b590b76efee770442b1
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-05-29 11:24:54 -0500

    Working throughout

--- src/make_network_maps.m
+++ src/make_network_maps.m
@@ -2,6 +2,7 @@ function make_network_maps( ...
 	out_dir, ...
 	wfiltered_fmri_nii, ...
 	wmeanfmri_nii, ...
+	mask_nii, ...
 	edge_nii, ...
 	project, ...
 	subject, ...
@@ -21,8 +22,8 @@ function make_network_maps( ...
 copyfile( ...
 	which('Yeo2011_7Networks_MNI152_FreeSurferConformed1mm_LiberalMask.nii.gz'), ...
 	fullfile(out_dir,'Yeo.nii.gz'));
-system(['gunzip -f ' outdir '/Yeo.nii.gz']);
-yeo_nii = [outdir '/Yeo.nii'];
+system(['gunzip -f ' out_dir '/Yeo.nii.gz']);
+yeo_nii = [out_dir '/Yeo.nii'];
 flags = struct('mask',true,'mean',false,'interp',0,'which',1,'wrap',[0 0 0], ...
         'prefix','r');
 spm_reslice({[wfiltered_fmri_nii ',1']; yeo_nii},flags);
@@ -38,16 +39,20 @@ redge_nii = fullfile(p,['r' n e]);
 Vedge = spm_vol(redge_nii);
 Yedge = spm_read_vols(Vedge);
 
+% And for mask
+spm_reslice({[wfiltered_fmri_nii ',1']; mask_nii},flags);
+[p,n,e] = fileparts(mask_nii);
+rmask_nii = fullfile(p,['r' n e]);
+Vmask = spm_vol(rmask_nii);
+Ymask = spm_read_vols(Vmask);
+keeps = Ymask(:)>0;
+
 % Load filtered fMRI data
 Ymeanfmri = spm_read_vols(spm_vol(wmeanfmri_nii));
 Yfmri = spm_read_vols(spm_vol(wfiltered_fmri_nii));
 osize = size(Yfmri);
 Yfmri = reshape(Yfmri,[],osize(4))';
 
-% Compute mean fmri and find a threshold for in-brain voxels
-thresh = spm_antimode(Ymeanfmri(:)) / 2;
-Ymask = Ymeanfmri >= thresh;
-keeps = Ymask(:);
 
 % Extract time series for each system
 warning('off','MATLAB:table:RowsAddedExistingVars')
@@ -60,10 +65,10 @@ systems.Label(end+1,1) = 5; systems.System{end,1} = 'Limbic';
 systems.Label(end+1,1) = 6; systems.System{end,1} = 'FrontoParietal';
 systems.Label(end+1,1) = 7; systems.System{end,1} = 'DefaultMode';
 
-maps = zeros(length(systems),length(Ymeanfmri(:)));
-for sy = 1:length(systems)
+maps = zeros(height(systems),length(Ymeanfmri(:)));
+for sy = 1:height(systems)
 	
-	roidata = Yfmri(:,Ymask(:) & Yyeo==systems.Label(sy));
+	roidata = Yfmri(:,Ymask(:) & Yyeo(:)==systems.Label(sy));
 	roimean = mean(roidata,2);
 	maps(sy,keeps) = corr(roimean,Yfmri(:,keeps));
 	
@@ -76,7 +81,7 @@ for sy = 1:length(systems)
 	
 end
 
-msize = [osize(1:3) length(systems)];
+msize = [osize(1:3) height(systems)];
 maps = reshape(maps',msize);
 
 % PDF figures
@@ -94,7 +99,7 @@ else
 	dh = ssw / ratio;
 end
 
-for sy = 1:length(systems)
+for sy = 1:height(systems)
 	
 	% Create figure
 	pdf_figure = openfig('pdf_connmaps_figure.fig','new');
@@ -104,12 +109,12 @@ for sy = 1:length(systems)
 	
 	% Summary
 	set(figH.summary_text, 'String', ...
-		sprintf('%s',systems{sy}) )
+		sprintf('%s',systems.System{sy}) )
 	
 	% Scan info
 	set(figH.scan_info, 'String', sprintf( ...
-		'%s: %s, %s, %s, %s', ...
-		filetag, project, subject, session, scan));
+		'%s, %s, %s, %s', ...
+		project, subject, session, scan));
 	set(figH.date,'String',['Report date: ' date]);
 	set(figH.version,'String',['Matlab version: ' version]);
 	
@@ -138,7 +143,7 @@ for sy = 1:length(systems)
 	end
 	
 	% Print to PNG
-	print(gcf,'-dpng',sprintf('%s/connmap_%s_%03d.png',out_dir,filetag,sy))
+	print(gcf,'-dpng',sprintf('%s/connmap_%s.png',out_dir,systems.System{sy}))
 	close(pdf_figure)
 	
 end
