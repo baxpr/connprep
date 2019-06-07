c6181f87395dfa1b0b47b283a1036bf5e688c4c2
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2019-06-07 12:03:06 -0500

    Fix ICV save location, zip edge image

--- src/mask_mni.m
+++ src/mask_mni.m
@@ -4,10 +4,14 @@ for f = 1:length(fmri_niis)
 
 	fmri_nii = fmri_niis{f};
 
-	% Resample ICV to fMRI space and load
+	% Resample ICV to fMRI space and load. Copy to out_dir first so we
+	% aren't trying to store the resliced image in a nonwriteable part of
+	% the container
 	flags = struct('mask',true,'mean',false,'interp',0,'which',1, ...
         'wrap',[0 0 0],'prefix','r');
 	icv_nii = [spm('dir') '/tpm/mask_ICV.nii'];
+	copyfile(icv_nii,fullfile(out_dir,'mask_ICV.nii'));
+	icv_nii = fullfile(out_dir,'mask_ICV.nii');
 	spm_reslice({[fmri_nii ',1'],icv_nii},flags);
 	[p,n,e] = fileparts(icv_nii);
 	ricv_nii = fullfile(p,['r' n e]);
